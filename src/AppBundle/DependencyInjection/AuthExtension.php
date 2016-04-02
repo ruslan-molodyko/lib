79f55ca53e7d269645efa318f3cbee9e0c340cf0
ruslan-molodyko <ruslan.molodyko@gmail.com>
2016-04-02 06:09:35 +0300

    Moved routing to appropriate bundle

--- src/AppBundle/DependencyInjection/AuthExtension.php
+++ src/AppBundle/DependencyInjection/AuthExtension.php
@@ -13,7 +13,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class AuthExtension extends Extension
+class AppExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
