a3dfecd9ec4faf01e8d28d68df097a4e5c6c99ab
ruslan-molodyko <ruslan.molodyko@gmail.com>
2016-04-02 03:29:03 +0300

    Ended login

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadUserData implements FixtureInterface, ContainerAwareInterface
+{
+    /** @var  ContainerInterface */
+    public $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+
+        $encoder = $this->container->get('security.password_encoder');
+        $encoded = $encoder->encodePassword($user, '123456');
+
+        $user
+            ->setEmail('molodyko13@gmail.com')
+            ->setPassword($encoded);
+
+        $manager->persist($user);
+
+        $user = new User();
+
+        $encoder = $this->container->get('security.password_encoder');
+        $encoded = $encoder->encodePassword($user, '123456');
+
+        $user
+            ->setEmail('molodyko15@gmail.com')
+            ->setPassword($encoded);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
---
+++ src/AppBundle/Entity/Admin.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Table(name="admin")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AdminRepository")
+ */
+class Admin implements UserInterface, \Serializable
+{
+    /** Full access rights */
+    const ROLE_ADMIN = 'ROLE_ADMIN';
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=60, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @ORM\Column(name="roles", type="json_array")
+     */
+    private $roles = [self::ROLE_ADMIN];
+
+    public function __construct()
+    {
+        $this->isActive = true;
+    }
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param array $roles
+     */
+    public function setRoles($roles = [self::ROLE_ADMIN])
+    {
+        $this->roles = $roles;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    /** @see \Serializable::serialize() */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->email,
+            $this->password
+        ));
+    }
+
+    /** @see \Serializable::unserialize() */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->email,
+            $this->password
+            ) = unserialize($serialized);
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     *
+     * @return User
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+}
